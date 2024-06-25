010b1475c94eb96b77a34a52bad6359c71b20703
Sarawut Sangsuwan <125952303+Sarawut2745@users.noreply.github.com>
2024-06-25 10:34:57 +0700

    Connet

--- connet.php
+++ connet.php
@@ -7,7 +7,7 @@ $databese = "shop";
 $conn = new mysqli($servername, $username, $password, $databese);
 
 if ($conn->connect_error) {
-    die("". $conn->connect_error);
+    die("Conn Fail". $conn->connect_error);
 }
 echo"Connected successfully";
 ?>
